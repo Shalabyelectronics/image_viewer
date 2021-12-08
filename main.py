67519b0cdff27e464d5244a5ed0bc5c2725d92db
shalabyelectronics <mhdshalabystar@gmail.com>
2021-12-08 04:46:19 +0300

    add image status.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 # -------------Set-Up my windows ----------#
-# ----Color
+# ----Color  and Fonts --------#
+FONT = "Coiny"
 BACKGROUND_COLOR = "#20574f"
+FORGROUND = "#62d9c7"
 # ------Window
 root = Tk()
 root.title("Image Viewer")
@@ -27,26 +29,32 @@ backward_img = PhotoImage(file="img/fast-backwardr.png")
 exit_img = PhotoImage(file="img/exitr.png")
 # ---------------Image viewer Function----------#
 index_count = 0
+status_number = 1
 
 
+# ----------Go forward Function ------------#
 def go_forward():
-    global index_count
+    global status_number, index_count
     index_count += 1
-    try:
+    status_number += 1
+    if index_count < len(images_list):
+        status_bar.config(text=f"Image {status_number} of {len(images_list)}")
         canvas.itemconfig(view_image, image=images_list[index_count])
-    except IndexError:
-        index_count = 0
-        canvas.itemconfig(view_image, image=images_list[index_count])
+    else:
+        index_count = 6
+        status_number = 7
 
 
-# ------------Go back word Function------------#
+# ----------Go backward Function ------------#
 def go_backward():
-    global index_count
+    global status_number, index_count
     index_count -= 1
-    try:
-        canvas.itemconfig(view_image, image=images_list[index_count])
-    except IndexError:
-        index_count = -1
+    status_number -= 1
+    if index_count < 0:
+        index_count = 0
+        status_number = 1
+    else:
+        status_bar.config(text=f"Image {status_number} of {len(images_list)}")
         canvas.itemconfig(view_image, image=images_list[index_count])
 
 
@@ -58,11 +66,14 @@ forward_b = Button(image=forward_img, bg=BACKGROUND_COLOR, activebackground=BACK
 backward_b = Button(image=backward_img, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, border=0,
                     command=go_backward)
 exit_b = Button(image=exit_img, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR, border=0, command=root.quit)
+status_bar = Label(text=f"Image 1 of {len(images_list)}", bd=2, relief=SUNKEN, bg=BACKGROUND_COLOR, fg=FORGROUND,
+                   font=(FONT, 15), pady=5)
 # --------Grid widgets positions---------#
 
 canvas.grid(column=0, row=0, columnspan=3, padx=30, pady=15)
 backward_b.grid(column=0, row=1, pady=10, sticky=E)
 forward_b.grid(column=2, row=1, pady=10, sticky=W)
 exit_b.grid(column=1, row=1)
+status_bar.grid(column=0, row=2, columnspan=3,sticky=W+E)
 
 root.mainloop()
